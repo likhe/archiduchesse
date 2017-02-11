a2822c1efab3d0612be70679c70a10951458a5a9
likhe <likhelifi@laposte.net>
2017-02-11 08:49:20 +0100

    flex the main products, hv to check for a sticky footer

---
+++ basket.js
@@ -0,0 +1,21 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function(event)) {
+  //define the targets
+  let contentBasket = document.getElementById('basket');
+  let buySocks = document.querySelectorAll('buysocks');
+  let nbBasket = 0;
+  //create an addEventListener: when the link of img is clicked, targets which targets the basket
+  for (let i = 0; i < buySocks.length; i++) {
+      buySocks[i].addEventListener('click', function() {
+          contentBasket.document.write(nbBasket++);
+      });
+  }
+
+  for (let j = 0; j < contentBasket.length; j++) {
+      contentBasket[i].addEventListener('click', function() {
+          contentBasket.innerHTML = nbBasket++;
+      });
+  }
+
+
+}
